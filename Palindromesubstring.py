cbff5e10843840931ca7dc46adf90fcc0aa63f24
ManasBasa <manasbasa@Manass-MacBook-Air.local>
2022-04-02 13:44:07 -0700

    Leetcode PalindromeSubstring problem

---
+++ Palindromesubstring.py
@@ -0,0 +1,25 @@
+# Given a string s, return the longest palindromic substring in s.
+# example babad
+
+import time
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        self.s = s
+        if len(self.s) == 1:
+            return (self.s)
+        mylist = []
+        for j in range(1, len(self.s) + 1):
+            for i in range(0, len(self.s)):
+                element = s[i:j + i]
+                if element == element[::-1]:
+                    mylist.append(element)
+        if (len(mylist) > 0):
+            return (sorted(mylist, key=len)[-1])
+
+
+A = Solution()
+b = A.longestPalindrome(
+    "lcnvoknqgejxbfhijmxglisfzjwbtvhodwummdqeggzfczmetrdnoetmcydwddmtubcqmdjwnpzdqcdhplxtezctvgnpobnnscrmeqkwgiedhzsvskrxwfyklynkplbgefjbyhlgmkkfpwngdkvwmbdskvagkcfsidrdgwgmnqjtdbtltzwxaokrvbxqqqhljszmefsyewwggylpugmdmemvcnlugipqdjnriythsanfdxpvbatsnatmlusspqizgknabhnqayeuzflkuysqyhfxojhfponsndytvjpbzlbfzjhmwoxcbwvhnvnzwmkhjxvuszgtqhctbqsxnasnhrusodeqmzrlcsrafghbqjpyklaaqximcjmpsxpzbyxqvpexytrhwhmrkuybtvqhwxdqhsnbecpfiudaqpzsvfaywvkhargputojdxonvlprzwvrjlmvqmrlftzbytqdusgeupuofhgonqoyffhmartpcbgybshllnjaapaixdbbljvjomdrrgfeqhwffcknmcqbhvulwiwmsxntropqzefwboozphjectnudtvzzlcmeruszqxvjgikcpfclnrayokxsqxpicfkvaerljmxchwcmxhtbwitsexfqowsflgzzeynuzhtzdaixhjtnielbablmckqzcccalpuyahwowqpcskjencokprybrpmpdnswslpunohafvminfolekdleusuaeiatdqsoatputmymqvxjqpikumgmxaxidlrlfmrhpkzmnxjtvdnopcgsiedvtfkltvplfcfflmwyqffktsmpezbxlnjegdlrcubwqvhxdammpkwkycrqtegepyxtohspeasrdtinjhbesilsvffnzznltsspjwuogdyzvanalohmzrywdwqqcukjceothydlgtocukc")
+print(b)
