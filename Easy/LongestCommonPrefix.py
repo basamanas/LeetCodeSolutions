6832be385e9dda91fca1ae43f26a2bef54d42648
Manas Basa <manasbasa@Manass-MacBook-Air.local>
2023-08-19 16:54:35 -0700

    LongestCommon prefix

---
+++ Easy/LongestCommonPrefix.py
@@ -0,0 +1,30 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        lengthofStrings = []
+        stringtoreturn = ""
+        for i in strs:
+            lengthofStrings.append(len(i))
+        lengthofStrings.sort()
+        lengthoflargeststring = lengthofStrings[0]
+        for j in range(0, lengthoflargeststring):
+            for i in range(0, len(strs) - 1):
+                if strs[i][j] == strs[i + 1][j]:
+                    continue
+                else:
+                    return stringtoreturn
+            stringtoreturn = stringtoreturn + strs[0][j]
+        return stringtoreturn
+
+
+A = Solution()
+result = A.longestCommonPrefix([])
+print(result)
